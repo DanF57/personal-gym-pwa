9114488fed19f2aecd419ae24d073b4ae88d87d1
DanF57 <danser2002@hotmail.com>
2026-04-07 16:59:40 -0500

    fix: manifest logos

--- vite.config.js
+++ vite.config.js
@@ -20,22 +20,16 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icons/logo.jpg',
-            sizes: '203x203',
-            type: 'image/jpeg',
-            purpose: 'any'
-          },
-          {
             src: 'icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'maskable'
+            purpose: 'any'
           },
           {
             src: 'icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'maskable'
+            purpose: 'any maskable'
           }
         ]
       }
